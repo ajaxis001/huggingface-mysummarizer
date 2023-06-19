dc2d8d7b154d5325ff56a5a2f80111235436e024
ajaxis001 <20327774+ajaxis001@users.noreply.github.com>
2023-06-19 23:51:28 +0000

    Event: DuplicateError persists Action: Edit app.py change code Observation: Newer versions of Gradio dont use Blocks interface anymore

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-with gr.Blocks() as demo:
-    textbox = gr.inputs.Textbox(placeholder="Enter text block to summarize", lines=4)
-    output_text = gr.outputs.Textbox()
-    gr.Interface(fn=predict, inputs=textbox, outputs=output_text)
+demo = gr.Interface(
+  fn=predict, 
+  inputs='text',
+  outputs='text',
+)
 
-demo.launch() 
+demo.launch()
